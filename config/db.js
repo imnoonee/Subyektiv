d5a273beb78c9a6d0f8c1cd792e4607df1485748
Otabek <burhonovotabek5@gmail.com>
2025-05-05 14:43:38 +0500

    second

--- config/db.js
+++ config/db.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Supabase SSL talab qiladi
-  },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
+  // Shared pooler PREPARE ni qo‘llab-quvvatlamaydi, shuning uchun tayyor holatda ishlatamiz
 });
 
 module.exports = pool;
